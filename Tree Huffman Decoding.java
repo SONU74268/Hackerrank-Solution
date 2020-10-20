784b4582d65865a043eb077a87047856847ef690
SONU74268 <shantilal3377@gmail.com>
2020-10-20 23:28:55 +0530

    adding the huffman decoding ans

---
+++ Tree Huffman Decoding.java
@@ -0,0 +1,63 @@
+import java.util.*;
+ 
+abstract class Node implements Comparable<Node> {
+    
+  	public  int frequency; // the frequency of this tree
+    public  char data;
+    public  Node left, right; 
+    public Node(int freq) { 
+    	frequency = freq;
+   	}
+ 
+    // compares on the frequency
+    public int compareTo(Node tree) {
+        return frequency - tree.frequency;
+    }
+  
+}
+ 
+class HuffmanLeaf extends Node {
+    
+    public HuffmanLeaf(int freq, char val) {
+        super(freq);
+        data = val;
+    }
+  
+}
+ 
+class HuffmanNode extends Node {
+    
+    public HuffmanNode(Node l, Node r) {
+        super(l.frequency + r.frequency);
+        left = l;
+        right = r;
+    }
+
+}
+
+
+class Decoding {
+
+/*  
+	class Node
+		public  int frequency; // the frequency of this tree
+    	public  char data;
+    	public  Node left, right;
+    
+*/ 
+    
+    void decode(String s, Node root) {
+        
+        char[] strChar = s.toCharArray();
+        Node traverese = root;
+        String decode = "";
+        for (int i = 0; i < strChar.length; i++) {
+            traverese = strChar[i] == '1' ? traverese.right : traverese.left;
+            if (traverese.left == null && traverese.right == null) {
+                decode += traverese.data;
+                traverese = root;
+            }
+        }
+        System.out.println(decode);
+    }
+
